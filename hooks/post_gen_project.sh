13c198643c7cd07b035572f71ebe95cfc726387c
Travis Bumgarner <travis.bumgarner@gmail.com>
2020-11-16 13:24:03 -0500

    updated build script with specific versions

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,8 +4,8 @@ cd ..
 mv ./{{cookiecutter.project_name}} ..
 cd ../{{cookiecutter.project_name}}
 
-npm i react react-dom styled-components
-npm i -D webpack webpack-cli html-webpack-plugin typescript ts-loader @types/react @types/react-dom webpack-dev-server
+npm i react@16 react-dom@16 styled-components@5
+npm i -D webpack@4 webpack-cli@3 html-webpack-plugin@3 typescript@3 ts-loader@7 @types/react@16 @types/react-dom@16 webpack-dev-server@3
 
 code .
 
