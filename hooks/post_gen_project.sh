f97711369543ccbb243a4555b581d091b19b6e4a
Travis Bumgarner <travis.bumgarner@gmail.com>
2021-12-21 15:30:33 -0700

    doing some refactoring

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,25 +1,10 @@
 #!/bin/sh
 
 cd ..
-mv ./{{cookiecutter.project_name}} ..
-cd ../{{cookiecutter.project_name}}
+mv ./{{cookiecutter.folder_name}} ..
+cd ../{{cookiecutter.folder_name}}
 
-npm i \
-    react@16 \
-    react-dom@16 \
-    styled-components@5 \
-
-
-npm i -D \
-    webpack@5 \
-    webpack-cli@4 \
-    html-webpack-plugin@5 \
-    typescript@4 \
-    ts-loader@9 \
-    @types/react@17 \
-    @types/react-dom@17 \
-    @types/styled-components@5 \
-    webpack-dev-server@3 \
+npm i
 
 code .
 
