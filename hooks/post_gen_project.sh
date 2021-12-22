817a93662371890aa79ef9e349edb29a37f03466
Travis Bumgarner <travis.bumgarner@gmail.com>
2021-12-22 09:43:17 -0700

    merge

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -12,15 +12,11 @@ mv ./{{cookiecutter.backend_folder}} $DESTINATION
 
 cd $DESTINATION
 
-<<<<<<< HEAD
-cd {{cookiecutter.frontend_folder}} 
-=======
 # Make Empty Folders
 mkdir ./src/db/seeds 
 mkdir ./src/db/migrations 
 
 cd {{cookiecutter.backend_folder}}
->>>>>>> 02705328f4791d3dceb65249b1307880c0e9054f
 
 npm i
 
