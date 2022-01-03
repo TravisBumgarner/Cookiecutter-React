c8530143f42933474a67dc6b5a1d8fb5f3e19a5f
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-01-03 08:46:45 -0700

    tweaking post hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
 
-cd ..
-
-DESTINATION="{{cookiecutter.project_parent}}/{{cookiecutter.project_name}}"
-
-echo "Moving to ${DESTINATION}"
-
-mkdir -p $DESTINATION
-
-mv ./{{cookiecutter.frontend_folder}} $DESTINATION
-
-cd $DESTINATION
-
-cd {{cookiecutter.frontend_folder}} 
-
 npm i
 
 exit 0
