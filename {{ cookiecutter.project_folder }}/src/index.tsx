e1c2f0634c0bac856983364051e9278f34dfd7f0
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-12-26 14:07:05 -0500

    bug fix for missing context

--- {{ cookiecutter.project_folder }}/src/index.tsx
+++ {{ cookiecutter.project_folder }}/src/index.tsx
@@ -1,12 +1,15 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+import ResultsContext from 'context';
 import App from './App'
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <ResultsContext>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </ResultsContext>
 );
